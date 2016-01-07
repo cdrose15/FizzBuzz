dd557f8719ee01bf71885f9ae69b9e3bd86cf804
Chris Rose <cdrose15@gmail.com>
2016-01-06 23:15:24 -0500

    FizzBuzz
    1st upload

---
+++ multiple.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function() {
+
+    $("#submit").click(function () {
+
+        var x = $("#a").val();
+        var y = $("#b").val();
+
+        for (var i = 0; i <= 100; i++) {
+
+            if ((i % x == 0) && (i % y == 0)) {
+                $("#result").append("FizzBuzz" + " ");
+            }
+            else if (i % x == 0) {
+                $("#result").append("Fizz" + " ");
+            }
+            else if (i % y == 0) {
+                $("#result").append("Buzz" + " ");
+            }
+            else {
+                $("#result").append(i + " ");
+            }
+        }
+    });
+});
